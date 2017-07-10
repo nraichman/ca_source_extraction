8b0d6e9ec2032d1a745fe25c7da08f5e7092833a
nraichman <raichman8@gmail.com>
2017-07-10 12:27:45 -0400

    Some comments on demo_script

--- demo_script.m
+++ demo_script.m
@@ -1,15 +1,15 @@
 clear;
 %% load file
 
-addpath(genpath('utilities'));
-             
-nam = 'demoMovie.tif';          % insert path to tiff stack here
+addpath(genpath('utilities'));%Nadav: Not sure what the result of this function call is.
+
+nam = 'demoMovie.tif';          % insert path to tiff stack here %Nadav: The default example path doesn't make sense.
 sframe=1;						% user input: first frame to read (optional, default 1)
 num2read=2000;					% user input: how many frames to read   (optional, default until the end)
 Y = bigread2(nam,sframe,num2read);
 
-%Y = Y - min(Y(:)); 
-if ~isa(Y,'double');    Y = double(Y);  end         % convert to single
+%Y = Y - min(Y(:));
+if ~isa(Y,'double');    Y = double(Y);  end         % convert to single %%Nadav: This function converts to double.
 
 [d1,d2,T] = size(Y);                                % dimensions of dataset
 d = d1*d2;                                          % total number of pixels
@@ -17,15 +17,15 @@ d = d1*d2;                                          % total number of pixels
 %% Set parameters
 
 K = 40;                                           % number of components to be found
-tau = 4;                                          % std of gaussian kernel (size of neuron) 
+tau = 4;                                          % std of gaussian kernel (size of neuron)
 p = 2;                                            % order of autoregressive system (p = 0 no dynamics, p=1 just decay, p = 2, both rise and decay)
 merge_thr = 0.8;                                  % merging threshold
 
-options = CNMFSetParms(...                      
+options = CNMFSetParms(...
     'd1',d1,'d2',d2,...                         % dimensions of datasets
     'search_method','dilate','dist',3,...       % search locations when updating spatial components
     'deconv_method','constrained_foopsi',...    % activity deconvolution method
-    'temporal_iter',2,...                       % number of block-coordinate descent steps 
+    'temporal_iter',2,...                       % number of block-coordinate descent steps
     'fudge_factor',0.98,...                     % bias correction for AR coefficients
     'merge_thr',merge_thr,...                    % merging threshold
     'gSig',tau...
@@ -51,7 +51,7 @@ refine_components = false;  % flag for manual refinement
 if refine_components
     [Ain,Cin,center] = manually_refine_components(Y,Ain,Cin,center,Cn,tau,options);
 end
-    
+
 %% update spatial components
 Yr = reshape(Y,d,T);
 [A,b,Cin] = update_spatial_components(Yr,Cin,fin,[Ain,bin],P,options);
@@ -67,9 +67,9 @@ P.p = 0;    % set AR temporarily to zero for speed
 run_GUI = false;
 if run_GUI
     Coor = plot_contours(A,Cn,options,1); close;
-    GUIout = ROI_GUI(A,options,Cn,Coor,keep,ROIvars);   
+    GUIout = ROI_GUI(A,options,Cn,Coor,keep,ROIvars);
     options = GUIout{2};
-    keep = GUIout{3};    
+    keep = GUIout{3};
 end
 %% merge found components
 [Am,Cm,K_m,merged_ROIs,Pm,Sm] = merge_components(Yr,A(:,keep),b,C(keep,:),f,P,S,options);
@@ -82,13 +82,13 @@ if and(display_merging, ~isempty(merged_ROIs))
     figure;
         set(gcf,'Position',[300,300,(ln+2)*300,300]);
         for j = 1:ln
-            subplot(1,ln+2,j); imagesc(reshape(A(:,merged_ROIs{i}(j)),d1,d2)); 
+            subplot(1,ln+2,j); imagesc(reshape(A(:,merged_ROIs{i}(j)),d1,d2));
                 title(sprintf('Component %i',j),'fontsize',16,'fontweight','bold'); axis equal; axis tight;
         end
         subplot(1,ln+2,ln+1); imagesc(reshape(Am(:,K_m-length(merged_ROIs)+i),d1,d2));
-                title('Merged Component','fontsize',16,'fontweight','bold');axis equal; axis tight; 
+                title('Merged Component','fontsize',16,'fontweight','bold');axis equal; axis tight;
         subplot(1,ln+2,ln+2);
-            plot(1:T,(diag(max(C(merged_ROIs{i},:),[],2))\C(merged_ROIs{i},:))'); 
+            plot(1:T,(diag(max(C(merged_ROIs{i},:),[],2))\C(merged_ROIs{i},:))');
             hold all; plot(1:T,Cm(K_m-length(merged_ROIs)+i,:)/max(Cm(K_m-length(merged_ROIs)+i,:)),'--k')
             title('Temporal Components','fontsize',16,'fontweight','bold')
         drawnow;
